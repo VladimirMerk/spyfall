d59a8cfa77155bd24d315a467b0bc2d275c69302
VMerk <mikulenko.vladimir@yandex.ru>
2020-07-02 18:29:31 +0300

    Initial

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# XOR encoding on bitmap
+
+Данный пример показывает расшифровку ч/б изображения зашифрованного методом XOR-ширования с использованием ключа и закодированного в цветное изображение.
+
+https://spyfall.javascript.ninja/
+
+Пример: https://vmerk.com/samples/spyfall
---
+++ functions.js
@@ -0,0 +1,70 @@
+const sourceCtx = document.getElementById('source').getContext('2d');
+const messageCtx = document.getElementById('message').getContext('2d');
+const solverCtx = document.getElementById('solver').getContext('2d');
+
+function getEncryptionKey() {
+  return window.encryptionKey;
+}
+
+function getOriginalPixelColor(x, y) {
+  [r, g, b] = sourceCtx.getImageData(x, y, 1, 1).data;
+  return { r, g, b };
+}
+
+function getMessagePixelColor(x, y) {
+  [r, g, b] = messageCtx.getImageData(x, y, 1, 1).data;
+  return { r, g, b };
+}
+
+let pendingSolution = null;
+function putPixelToSolution({ r, g, b }, x, y) {
+  if (!pendingSolution) {
+    pendingSolution = solverCtx.getImageData(0, 0, width, height);
+    setTimeout(() => {
+      solverCtx.putImageData(pendingSolution, 0, 0);
+      pendingSolution = null;
+    });
+  }
+
+  pendingSolution.data[(y * width + x) * 4] = r;
+  pendingSolution.data[(y * width + x) * 4 + 1] = g;
+  pendingSolution.data[(y * width + x) * 4 + 2] = b;
+  pendingSolution.data[(y * width + x) * 4 + 3] = 255;
+}
+
+const img = new Image();
+img.src = window.sourceImage;
+img.addEventListener('load', () => {
+  sourceCtx.drawImage(img, 0, 0);
+});
+
+const img2 = new Image();
+img2.src = window.messageImage;
+img2.addEventListener('load', () => {
+  messageCtx.drawImage(img2, 0, 0);
+});
+
+function subtraction(pixelA, pixelB) {
+  return {
+    r: pixelA.r - pixelB.r,
+    g: pixelA.g - pixelB.g,
+    b: pixelA.b - pixelB.b,
+  };
+}
+
+function pixelToCipherBits(pixel) {
+  return Object.values(pixel)
+    .map((byte) => byte.toString(2))
+    .join('');
+}
+
+function codeXorBits(cipherBits, keyBits) {
+  const cipherBitsArray = cipherBits.split('');
+  const keyBitsArray = keyBits.split('');
+  return cipherBitsArray
+    .map((bit, i) => {
+      const keyBit = keyBitsArray[i];
+      return bit ^ keyBit;
+    })
+    .join('');
+}
